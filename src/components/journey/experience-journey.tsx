278235aca92f2f5ad099c3e76ec7abb2e01e70a6
Raphtaliak <miranasiam023@gmail.com>
2024-11-25 14:26:27 +0700

    add:journey details

--- src/components/journey/experience-journey.tsx
+++ src/components/journey/experience-journey.tsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import { TbArrowBadgeRightFilled } from "react-icons/tb";
 import { cn } from "../../utils/cn";
+import { journeyList } from "../../utils/journey-data";
 const ExperienceJourney = () => {
   const [type, setType] = useState(1);
 
+  console.log(
+    journeyList.map((j) => {
+      console.log(j);
+    }),
+  );
+
   return (
     <>
-      <div className="w-[100%] px-20 ">
+      <div className="w-[100%] px-20">
         <div className="flex flex-col items-center justify-center gap-[10px]">
-          <p className="lg:pb-2 lg:text-[28px] ">02. Where I'm worked</p>
-          <div className="h-[0.5px] w-[200px] bg-white  bg-opacity-50"></div>
+          <p className="lg:pb-2 lg:text-[28px]">02. Where I'm worked</p>
+          <div className="h-[0.5px] w-[200px] bg-white bg-opacity-50"></div>
         </div>
         <div className="lower-part flex gap-[30px] pt-[40px]">
           <div className="tab flex h-[250px] flex-col gap-[4px] border-l-[0.5px] border-gray-500">
@@ -70,51 +77,51 @@ const ExperienceJourney = () => {
               Item5
             </div>
           </div>
-          {type == 1 && (
-            <div className="tab-details">
-              <div className="tab-title flex items-center gap-[10px]">
-                <h3 className="text-[20px]">Position </h3>
-                <h3 className="text-[20px]">Company Name</h3>
-              </div>
-              <p className="pt-[5px] text-[13px]">month year - month year</p>
-              <div className="tab-paragraph flex gap-[10px] lg:pt-[20px]">
-                <TbArrowBadgeRightFilled size="40" />
-                <p className="text-[14px]">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt
-                  blanditiis illum unde nostrum explicabo sequi perspiciatis.
-                  Laudantium non rem, dolore quos rerum magnam veritatis?
-                </p>
-              </div>
-              <div className="tab-paragraph flex gap-[10px] lg:pt-[20px]">
-                <TbArrowBadgeRightFilled size="40" />
-                <p className="text-[14px]">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt
-                  blanditiis illum unde nostrum explicabo sequi perspiciatis.
-                  Laudantium non rem, dolore quos rerum magnam veritatis?
-                </p>
-              </div>
-              <div className="tab-paragraph flex gap-[10px] lg:pt-[20px]">
-                <TbArrowBadgeRightFilled size="40" />
-                <p className="text-[14px]">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt
-                  blanditiis illum unde nostrum explicabo sequi perspiciatis.
-                  Laudantium non rem, dolore quos rerum magnam veritatis?
-                </p>
-              </div>
-              <div className="tab-paragraph flex gap-[10px] lg:pt-[20px]">
-                <TbArrowBadgeRightFilled size="40" />
-                <p className="text-[14px]">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt
-                  blanditiis illum unde nostrum explicabo sequi perspiciatis.
-                  Laudantium non rem, dolore quos rerum magnam veritatis?
-                </p>
-              </div>
-            </div>
-          )}
+          <div className="">
+            {/* {Journey?.map({position, company_name, id} )=> (
+            
+          )} */}
+            {journeyList.map((j) => (
+              <> {type == j.id && (
+                <div className="tab-details">
+                  <div className="tab-title flex items-center gap-[10px]">
+                    <h3 className="text-[20px]">{j.position} </h3>
+                    <h3 className="text-[20px]">{j.company_name}</h3>
+                  </div>
+                  <p className="pt-[5px] text-[13px]">{j.year_from_to}</p>
+                  <div className="tab-paragraph flex gap-[10px] lg:pt-[20px]">
+                    <TbArrowBadgeRightFilled size="40" />
+                    <p className="text-[14px]">
+                    {j.detail1}
+                    </p>
+                  </div>
+                  <div className="tab-paragraph flex gap-[10px] lg:pt-[20px]">
+                    <TbArrowBadgeRightFilled size="40" />
+                    <p className="text-[14px]">
+                    {j.detail2}
+                    </p>
+                  </div>
+                  <div className="tab-paragraph flex gap-[10px] lg:pt-[20px]">
+                    <TbArrowBadgeRightFilled size="40" />
+                    <p className="text-[14px]">
+                    {j.detail3}
+                    </p>
+                  </div>
+                  <div className="tab-paragraph flex gap-[10px] lg:pt-[20px]">
+                    <TbArrowBadgeRightFilled size="40" />
+                    <p className="text-[14px]">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt
+                      blanditiis illum unde nostrum explicabo sequi perspiciatis.
+                      Laudantium non rem, dolore quos rerum magnam veritatis?
+                    </p>
+                  </div>
+                </div>
+              )}</>
+            ))}
+          </div>
+         
+          
         </div>
       </div>
     </>
---
+++ src/utils/journey-data.ts
@@ -0,0 +1,72 @@
+export interface Journey {
+  position: string;
+  company_name: string;
+  year_from_to: string;
+  detail1: string;
+  detail2: string;
+  detail3: string;
+  id: number;
+}
+
+export const journeyList: Journey[] = [
+  {
+    position: "IT Instructor",
+    company_name: "LittleCode Lab",
+    year_from_to: "Aug 2022 - Feb 2023",
+    detail1:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    detail2:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    detail3:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    id: 1,
+  },
+  {
+    position: "IT Educator",
+    company_name: "Creative Codind Academy",
+    year_from_to: "Feb 2023 - Jun 2023",
+    detail1:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    detail2:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    detail3:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    id: 2,
+  },
+  {
+    position: "IT Educator",
+    company_name: "Creative Codind Academy",
+    year_from_to: "Feb 2023 - Jun 2023",
+    detail1:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    detail2:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    detail3:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    id: 3,
+  },
+  {
+    position: "IT Educator",
+    company_name: "Creative Codind Academy",
+    year_from_to: "Feb 2023 - Jun 2023",
+    detail1:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    detail2:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    detail3:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    id: 4,
+  },
+  {
+    position: "IT Educator",
+    company_name: "Creative Codind Academy",
+    year_from_to: "Feb 2023 - Jun 2023",
+    detail1:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    detail2:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    detail3:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, veniam voluptates! Voluptas nemo pariatur nesciunt blanditiis illum unde nostrum explicabo sequi perspiciatis. Laudantium non rem, dolore quos rerum magnam veritatis?",
+    id: 5,
+  },
+];
